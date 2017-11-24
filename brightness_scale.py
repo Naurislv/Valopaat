efc6458b530af7a6302819b201562791a343c1a8
Niina <niina.kajovuori@aalto.fi>
2017-11-24 21:27:51 +0200

    scaling brightness of the light

--- brightness_scale.py
+++ brightness_scale.py
@@ -1,3 +1,3 @@
 # a function for scaling 0...1 to 0...255 which corresponds the brightness of the light
 def brightness(input_value):
-    return (1.0/255.0) * input_value
+    return 255.0 * input_value
